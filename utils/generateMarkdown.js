118234807f3a38cb566f8ed6b94256744e5e8e22
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-06 16:00:33 -0700

    license link function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -36,7 +36,40 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = ""
+
+  switch(license) {
+    case 'GNU AGPLv3':
+      licenseLink= 'https://www.gnu.org/licenses/agpl-3.0'
+      break;
+    case 'GNU GPLv3':
+      licenseLink= 'https://www.gnu.org/licenses/gpl-3.0'
+      break;
+    case 'GNU LGPLv3':
+      licenseLink= 'https://www.gnu.org/licenses/lgpl-3.0'
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseLink= 'https://opensource.org/licenses/MPL-2.0'
+      break;
+    case 'Apache License 2.0':
+      licenseLink= 'https://opensource.org/licenses/Apache-2.0'
+      break;
+    case 'MIT License':
+      licenseLink= 'https://opensource.org/licenses/MIT'
+      break;   
+    case 'Boost Software License 1.0':
+      licenseLink= 'https://www.boost.org/LICENSE_1_0.txt'
+      break;
+    case 'The Unlicense':
+      licenseLink= 'http://unlicense.org/'
+      break;
+    default:
+      licenseLink = ""
+  }
+
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
