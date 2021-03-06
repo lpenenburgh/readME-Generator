4584ef133cf1c87bf45d9cd71f13ba2ebe6be470
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-03-06 15:14:11 -0700

    init function added

--- index.js
+++ index.js
@@ -68,9 +68,14 @@ function writeToFile(fileName, data) {
 
 
 // TODO: Create a function to initialize app
- function init() {
-
- }
+function init() {
+  inquirer.prompt(questions)
+  .then((response) => {
+      const markdown = generateMarkdown(response);
+      writeToFile('example.md', markdown);
+      
+  })
+}
 
 // Function call to initialize app
 init();
